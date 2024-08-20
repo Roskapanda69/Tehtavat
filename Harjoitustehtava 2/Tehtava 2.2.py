7b04a9eaad68b80d9c8f19260592ce5be975aa1e
Roskapanda69 <markku.pyrro@metropolia.com>
2024-08-20 18:14:45 +0300

    Eka lataus

---
+++ Harjoitustehtava 2/Tehtava 2.2.py
@@ -0,0 +1,5 @@
+import math
+sade = float(input ("Syötä ympyrän säde: "))
+pii = float (f"{math.pi}")
+pintaala = f"{sade**2*pii}"
+print(f"Ympyrän pinta-ala on {pintaala}")
---
+++ Harjoitustehtava 2/Tehtava 2.3.py
@@ -0,0 +1,6 @@
+import math
+kanta = float(input("Syötä suorakulmion kanta: "))
+korkeus = float(input("Syötä suorakulmion korkeus: "))
+pintaala = (kanta*korkeus)
+piiri = ((kanta+korkeus)*2)
+print(f"Suorakulmion pinta-ala on: {pintaala}\nSuorakulmion piiri on: {piiri}")
