74fc6c0b573b1f6794c17a981b46464aad71f9b6
Roskapanda69 <markku.pyrro@metropolia.com>
2024-08-21 10:59:34 +0300

    valmis?

--- Harjoitustehtava 2/Tehtava 2.6.py
+++ Harjoitustehtava 2/Tehtava 2.6.py
@@ -1,5 +1,7 @@
 import random
 print(f"Kolmi numeroinen koodi:")
-print (random.randint(0, 9),random.randint(0, 9),random.randint(0, 9))
-print(f"Nelinumeroinen koodi:")
-print (random.randint(1, 6),random.randint(1, 6),random.randint(1, 6),random.randint(1, 6))
+for i  in range(0,3):
+    print (random.randint(0, 9), end="")
+print(f"\nNelinumeroinen koodi:")
+for i in range(0,4):
+    print (random.randint(1, 6),end="")
