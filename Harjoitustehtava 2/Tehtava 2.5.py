02845364cb06831c3528f2ab95366651b509457a
Roskapanda69 <markku.pyrro@metropolia.com>
2024-08-21 08:37:57 +0300

    valmis?

--- Harjoitustehtava 2/Tehtava 2.5.py
+++ Harjoitustehtava 2/Tehtava 2.5.py
@@ -1,5 +1,8 @@
+import math
 leiviska = float(input("Anna leiviskät: "))
 naula = float(input("Anna naulat: "))
 luoti = float(input("Anna luodit: "))
-massakg = ((((((leiviska*20)+naula)*32)+luoti)*13.3)/1000)
-print(f"Massa on {massakg[:-10]}kg ja {massakg: 0.10f}g")
+massa = ((((((leiviska*20)+naula)*32)+luoti)*13.3))
+massakg = massa/1000
+massag = massa%1000
+print(f"Massa on nykymittojen mukaan: {massa:} \n{massakg:.0f} kilogrammaa ja {massag:3.2f} grammaa")
