489a6203322f15ab0fb9d52c52d4e5fab23abf04
Roskapanda69 <markku.pyrro@metropolia.com>
2024-08-27 12:54:35 +0300

    valmis?

--- Harjoitustehtavat 3/Tehtava_1.py
+++ Harjoitustehtavat 3/Tehtava_1.py
@@ -2,7 +2,7 @@
 # Jos kuha on alamittainen, ohjelma käskee laskea kuhan takaisin järveen ilmoittaen samalla käyttäjälle,
 # montako senttiä alimmasta sallitusta pyyntimitasta puuttuu. Kuha on alamittainen, jos sen pituus on alle 37 cm.
 
-pituus= float(input(f"Anna kuhan pituus sentteinä: "))
+pituus=float(input(f"Anna kuhan pituus sentteinä: "))
 if pituus<37:
               print(f"Laske kuha järveen. Kuha on {37-pituus}cm liian lyhyt.")
 else:
