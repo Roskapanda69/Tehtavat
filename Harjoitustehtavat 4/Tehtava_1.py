1affe9985856ce77912e8a6661d49278779651f1
Roskapanda69 <markku.pyrro@metropolia.com>
2024-08-27 14:20:11 +0300

    valmis?

---
+++ Harjoitustehtavat 4/Tehtava_1.py
@@ -0,0 +1,10 @@
+#Kirjoita while-toistorakennetta käyttävä ohjelma, joka tulostaa kolmella jaolliset luvut väliltä 1..1000.
+
+luku = 1
+
+while luku<1000:
+        while luku % 3 == 0:
+            print(f"{luku}")
+            break
+        luku = luku+1
+
