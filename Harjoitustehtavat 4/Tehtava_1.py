2db2f7d1e701b8951f1c65cdcf561e5bd7464f96
Roskapanda69 <markku.pyrro@metropolia.com>
2024-08-27 17:52:52 +0300

    valmis?

--- Harjoitustehtavat 4/Tehtava_1.py
+++ Harjoitustehtavat 4/Tehtava_1.py
@@ -1,7 +1,6 @@
 #Kirjoita while-toistorakennetta käyttävä ohjelma, joka tulostaa kolmella jaolliset luvut väliltä 1..1000.
 
 luku = 1
-
 while luku<1000:
         while luku % 3 == 0:
             print(f"{luku}")
---
+++ Harjoitustehtavat 4/Tehtava_3.py
@@ -0,0 +1,9 @@
+#Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka,
+#kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi.
+#Lopuksi ohjelma tulostaa saaduista luvuista pienimmän ja suurimman.
+
+
+luku = int(input(f"Anna luku:"))
+while luku !="":
+    break
+
