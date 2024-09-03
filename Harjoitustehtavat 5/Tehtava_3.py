1f0be3c70ef2215a170b0a5ecefb95b449ba9e8b
Roskapanda69 <markku.pyrro@metropolia.com>
2024-09-03 17:55:56 +0300

    valmis?

--- Harjoitustehtavat 5/Tehtava_3.py
+++ Harjoitustehtavat 5/Tehtava_3.py
@@ -3,21 +3,15 @@
 #Esimerkiksi luku 13 on alkuluku, koska se voidaan jakaa vain luvuilla 1 ja 13 siten, että jako menee tasan.
 #Toisaalta esimerkiksi luku 21 ei ole alkuluku, koska se voidaan jakaa tasan myös luvulla 3 tai luvulla 7.
 
-#määritellään range, jolla kokeillaan jaollisuus 1:stä omaan lukuun yhden numeron välein
-#jos jaollisuus == 0, luku on jaollinen kokeillulla luvulla. jos näitä on enemmän kuin kaksi, luku ei ole alkuluku
 
 luku = int(input(f"Anna luku :"))
-jakaa = 1
 jaot = 0
-jako = 0
-while jakaa <= luku :
-    for numero in range(jakaa, luku+1):
-        jakaa += 1
-        jako = luku/numero
-        if luku%numero!=0 and not jako==1 and not jako==luku:
-            jaot +=1
 
-if jaot==(luku-2):
+for numero in range(2, luku ):
+    if luku % numero == 0 :
+        jaot +=1
+
+if jaot==0:
     print(f"Luku on alukuluku")
 
 else:
