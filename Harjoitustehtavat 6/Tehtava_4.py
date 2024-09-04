de140f2ef8547c10535c79a89b3449f6b694dc70
Roskapanda69 <markku.pyrro@metropolia.com>
2024-09-04 12:22:09 +0300

    valmis?

--- Harjoitustehtavat 6/Tehtava_4.py
+++ Harjoitustehtavat 6/Tehtava_4.py
@@ -1,3 +1,17 @@
 #Kirjoita funktio, joka saa parametrinaan listan kokonaislukuja.
 #Ohjelma palauttaa listassa olevien lukujen summan. Kirjoita testausta varten pääohjelma, jossa luot listan,
 #kutsut funktiota ja tulostat sen palauttaman summan.
+
+def summaaja():
+    for n in listaa:
+        summa=n+n
+        return summa
+
+
+listaa= []
+
+while True:
+    numero= input("Anna summattavat luvut: ")
+    listaa.append(numero)
+else:
+    print(f"Lukjen summa on: {summaaja()}")
